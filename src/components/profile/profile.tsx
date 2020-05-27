dd708abbcf4d3eff0bf73827986eddc1a0392eca
Nikki <archiektor@gmail.com>
2020-05-27 16:20:00 +0200

    added first commit

---
+++ src/components/profile/profile.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import "./profile.css";
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://cdn57.androidauthority.net/wp-content/uploads/2015/11/00-best-backgrounds-and-wallpaper-apps-for-android.jpg"
+                    alt="attract photo"/>
+            </div>
+            <div>
+                ava + description
+                <img src="https://cs11.pikabu.ru/post_img/2020/05/06/11/158879081415286768.jpg" alt=""/>
+            </div>
+            <div>
+                my posts
+                <div>new post</div>
+            </div>
+            <div>
+                div for posts
+                <div>post1</div>
+                <div>post2</div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
