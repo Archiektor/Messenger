f69f53ac4e9f0f39bb73c3d16550d0d99e0e4afd
Nikki <archiektor@gmail.com>
2020-06-03 17:11:14 +0200

    initial commit

---
+++ src/components/music/music.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import css from "./news.module.css";
+
+const News = () => {
+    return (
+        <div>News</div>
+    )
+}
+
+export default News;
---
+++ src/components/news/news.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import css from "./dialogs.module.css";
+
+const Dialogs = () => {
+    console.log(css);
+    return (
+        <div >Dialogs</div>
+    )
+}
+
+export default Dialogs;
