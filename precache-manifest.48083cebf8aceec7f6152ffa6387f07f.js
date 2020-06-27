3ee1f8725c65188259ffbc0b6dc67230aea91c35
Nikki <archiektor@gmail.com>
2020-06-27 08:08:09 +0200

    Updates

---
+++ precache-manifest.48083cebf8aceec7f6152ffa6387f07f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed31940c278e68d29f341829256cbc4d",
+    "url": "/Archiektor/Messenger/index.html"
+  },
+  {
+    "revision": "19db9d1689de9e35d8af",
+    "url": "/Archiektor/Messenger/static/css/main.8b0b94f4.chunk.css"
+  },
+  {
+    "revision": "ddec4ee230469b2efeb0",
+    "url": "/Archiektor/Messenger/static/js/2.6fcfcbf0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Archiektor/Messenger/static/js/2.6fcfcbf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19db9d1689de9e35d8af",
+    "url": "/Archiektor/Messenger/static/js/main.4f6d0913.chunk.js"
+  },
+  {
+    "revision": "5aed507f568cebfe1479",
+    "url": "/Archiektor/Messenger/static/js/runtime-main.999b4ec0.js"
+  },
+  {
+    "revision": "f215c6c73ef373267acaa5218130fcf9",
+    "url": "/Archiektor/Messenger/static/media/person.f215c6c7.png"
+  }
+]);
